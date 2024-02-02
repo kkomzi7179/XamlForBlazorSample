77381faa35844f7ef3435546c84a2f897adc338a
Jaeyong Park <kkomzi@gmail.com>
2024-02-02 16:34:24 +0900

    - add xaml for blazor sample

---
+++ XamlForBlazorSample/XamlForBlazorUserControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+using static System.Net.WebRequestMethods;
+using static XamlForBlazorSample.Pages.Weather;
+
+namespace XamlForBlazorSample
+{
+	public partial class XamlForBlazorUserControl : UserControl
+	{
+		HttpClient Http = new HttpClient();
+		XamlForBlazorUserControlViewModel viewModel => this.DataContext as XamlForBlazorUserControlViewModel;
+
+		public XamlForBlazorUserControl()
+		{
+			this.InitializeComponent();
+
+			Http.BaseAddress = new Uri("https://localhost:7011");
+		}
+
+		private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			viewModel.Forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+		}
+	}
+	public class XamlForBlazorUserControlViewModel : INotifyPropertyChanged
+	{
+		public WeatherForecast[]? forecasts;
+		public WeatherForecast[]? Forecasts
+		{
+			get => forecasts;
+			set
+			{
+				if (forecasts == value) return;
+
+				forecasts = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string callerName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerName));
+		}
+	}
+}
